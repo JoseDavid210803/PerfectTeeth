c15309ef127ef3d7d06f02340a21e4a19c4b7b5b
Jose David Castellanos Mosqueda <jose.castellanos4652@alumnos.udg.mx>
2025-11-10 23:38:57 -0600

    nsure PostgreSQL config for Railway

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -85,7 +85,7 @@ if DATABASE_URL:
     DATABASES["default"] = dj_database_url.config(
         default=DATABASE_URL,
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True
     )
 # -----------------------------------------------------
 
