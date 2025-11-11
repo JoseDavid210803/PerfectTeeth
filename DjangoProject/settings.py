c306d588df842f7f3ea670f12f38315f52e3e6a6
Jose David Castellanos Mosqueda <jose.castellanos4652@alumnos.udg.mx>
2025-11-10 22:24:41 -0600

    configurar BD

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url  # <-- agregado
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cqfaca4z*=)rx+b97o%^2lco$n@^o9&o2p_9g+yuqtkz5w)e$u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["perfectteeth-production.up.railway.app"]
 
 
 # Application definition
@@ -80,6 +82,16 @@ DATABASES = {
     }
 }
 
+# --- CONEXIÓN AUTOMÁTICA A POSTGRESQL DE RAILWAY ---
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=False
+    )
+# ----------------------------------------------------
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -115,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+#PRODUCTION-------
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "elements" / "static"] 
+#PRODUCTION-------
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
